5045bc5f95f40b63763dfc33f6fc8defeb44f6a1
sampedersen <sam.mbped@gmail.com>
2025-04-23 14:53:01 -0400

    Updated for multiple chains

--- Code/RJAGS_current_core_v2.R
+++ Code/RJAGS_current_core_v2.R
@@ -48,46 +48,31 @@ se <- function(x) {
   sd(x) / sqrt(length(x))}
 
 #_______________________________________________________________________________
-#====================== 1a. User-Defined Settings ==============================
+#====================== 1a. Model settings ====== ==============================
 #_______________________________________________________________________________
-if (dir.exists("C:/Users/Sammb/")){
-  Device = "BM"
-} else if(dir.exists("C:/Users/Feede")){
-  Device = "FS"
-}
-# Condition 
-Condition = "HE"  # HE or WT 
-# Attempt Date
-Attempt_Date = format(Sys.Date(), "%B-%d")
-# Attempt Number 
-Attempt_Num = format(Sys.time(), "%H-%M")
-
-# Model Settings 
-# Red flags lol
-
-# Randomly sample a subset of the dataset instead of reducing sampling/thinning
-# Blair ~95k burnin
-# Increase 
 BurnIn = 10000 # First 10k samples we toss
 Sample = 2000  # Sample 2000 times
 Thinning = 10  # 1 per every 10 samples picked 
-Chains = 3    # 3 starting points (min) to converge at same end point 
+Chains = 6    # 3 starting points (min) to converge at same end point 
+# number of cores to recruit (1:1 core:chains)
+RcppParallel::setThreadOptions(Chains)
 
 # Data file name 
 data_filename = "Epoch4_Mid-to-Late_DDM_Data.xlsx"     
 
 #_______________________________________________________________________________
-#=============== 2. Set up directory paths & import data =======================
+#====================== 2. Directory Set-Up ===================================
 #_______________________________________________________________________________
-# Limit number of cores used for parallel processing based on number available per device 
-if (Device == "BM"){
+# Specify the device depending on user account directories that exist 
+if (dir.exists("C:/Users/Sammb/")){
+  Device = "BM"
   home_dir = path("C:/Users/Sammb/Documents/Sinai/Sweis Lab/Projects/Shank3")
   setwd(home_dir)
-} else {
+} else if(dir.exists("C:/Users/Feede")){
+  Device = "FS"
   home_dir = path("C:/Users/Feede/Documents/Sam_Temp/Shank3")
   setwd(home_dir)
-}
-RcppParallel::setThreadOptions(Chains)
+} else {print("Device directory not recognized.")}
 
 # Set up paths 
 data_path = home_dir / "Data"
@@ -96,10 +81,15 @@ output_path = home_dir / "Outputs"
 code_path = home_dir / "Code"
 
 # Load data
-# Note: edit this section depending on how the data is stored 
-# Check if file exists as .csv, if not check as .xlsx
 data_filepath = file.path(data_path, data_filename)    
-df_raw = read_excel(data_filepath)                    # use this for .xlsx
+df_raw = read_excel(data_filepath)                    
+
+
+# Attempt Date
+Attempt_Date = format(Sys.Date(), "%B-%d")
+# Attempt Number 
+Attempt_Num = format(Sys.time(), "%H-%M")
+
 
 #_______________________________________________________________________________
 #==========================    3. Pre-process data     =========================
@@ -112,20 +102,11 @@ df = df_raw %>%
          choice = `OZ outcome 1 or 0`,       # variable containing choice (coded, 1=accept, 0=reject)
          rt = `offer zone RT (s)`,           # variable containing rt in seconds
          offer = `offer`,        # variable containing offer amount
-         group = `genotype terminal`,
-         value = `offer value`) %>%    # variable participant's condition ('WT'=Wildtype, 'HT'=Shank3 het)
+         group = `genotype terminal`, # variable participant's condition ('WT'=Wildtype, 'HT'=Shank3 het)
+         value = `offer value`) %>%    
   select(subj_idx, group, day, trial, choice, rt, offer, value)
 
-# Remove RT Outliers         
-# Note: Hard limit cut-offs at [.25s,7s]
-# Note: Relative limits +/- 2SD of average RT per subject 
-
-# Consideration: Bin the response time acrose subjects (15,25ms) and see the timepoint that 
-# they're "correct" around 50%
-# Easy case where mice knows its preference, stronger true decision
-# Floor can def be lower than .25, read mice DDMs 
-
-###### COME BACK 
+# Remove RT Outliers (note: removed bottom limit, all RTs are under 2SDs)         
 df = df %>%
   group_by(subj_idx) %>%
   filter(rt < (mean(rt) + (2*sd(rt)))
@@ -134,7 +115,6 @@ df = df %>%
   ungroup()
 
 # Compute the number of trials lost due to RT filtering 
-# Note: ~5% loss in data is fine, shouldnt exceed ~8% 
 lossToCleaning <- (nrow(df_raw) - nrow(df)) / nrow(df_raw)
 print(paste0("Loss to cleaning: ", lossToCleaning*100, "%"))
 
@@ -143,25 +123,11 @@ idx <- which(df$choice == 0)  # Pull indices for rejected offer trials
 df$RT <- df$rt                # Duplicate RTs to a new column   
 df$RT[idx] <- df$rt[idx] * -1 # Update new column for signed RT values 
 
-# TAKE THIS OUT - DONT NEED RN 
-# Separate for training (80%) and testing (20%)
-#train_test_split <- df %>%            # Create a new dataframe 
-#  group_by(subj_idx,day) %>%          # Group by mouse and by day 
-#  mutate(Set = ifelse(runif(n()) < 0.8, "Train","Test"))
-#train_data <- filter(train_test_split,Set=="Train")   # Assign to training dataframe
-#test_data <- filter(train_test_split,Set=="Test")     # Assign to testing dataframe 
-
-# Reassign df as training_data for development and troubleshooting purposes 
-# Change this later, if needed 
-#df <- train_data
-
 #_______________________________________________________________________________
 #==================== # 4. Model fitting (DEVELOPMENT PHASE) ===================
 #_______________________________________________________________________________
+
 # Subsetting 
-# All mice (n=48)
-# Select across all days
-# 
 set.seed(22)
 selected_mice <- df %>%
   distinct(subj_idx,group) %>%    # Lists each mouse with condition info 
@@ -181,7 +147,10 @@ balanced_subset <- subset_df %>%
   sample_frac(.25) %>%
   ungroup()
 
-
+nrow(balanced_subset %>% filter(choice==0))
+nrow(balanced_subset %>% filter(choice==1))
+nrow(balanced_subset %>% filter(value_group=="pos"))
+nrow(balanced_subset %>% filter(value_group=="neg"))
 
 
 
@@ -194,25 +163,7 @@ balanced_subset <- subset_df %>%
 
 
 # 4. Model fitting (DEVELOPMENT PHASE) 
-
-# Filter based on user-identified group of interest 
-if(Condition == "HE"){
-  Data = df %>%
-    filter(group=="HE") %>%
-    mutate(idxP=as.numeric(ordered(subj_idx)))
-} else {
-  Data = df %>%
-    filter(group=="WT") %>%
-    mutate(idxP=as.numeric(ordered(subj_idx)))
-}
-
-
-
-
-
-
 for (genotype in unique(balanced_subset$group)) {
-  genotype="HE"
   Data = balanced_subset %>%
     filter(group==genotype) %>%
     mutate(idxP = as.numeric(ordered(subj_idx)))
@@ -235,148 +186,110 @@ for (genotype in unique(balanced_subset$group)) {
                           ns=ns))
   
   monitor = c("deviance",            # Come back to this 
-              # Group-level parameters (boundary, non-decision time, starting-point bias, effect of offer on drift rate)
-              "alpha.mu","theta.mu","bias.mu","b1.mu",
-              # Subject-level parameters
-              "alpha.p","theta.p","bias.p","b1.p")
+              "alpha.mu","theta.mu","bias.mu","b1.mu", # Group-level parameters (boundary, non-decision time, starting-point bias, effect of offer on drift rate)
+              "alpha.p","theta.p","bias.p","b1.p")    # Subject-level parameters
   
   inits1 <- dump.format(list(
-    alpha.mu=2, alpha.pr=.5,            # Alpha
-    theta.mu=0.200, theta.pr=0.5,       # Theta
-    b1.mu=0.16, b1.pr=0.1,              # Drift rate
-    bias.mu=0.52, bias.kappa=.5,        # Bias
+    alpha.mu=1.9, 
+    alpha.pr=.6,            # Alpha
+    theta.mu=0.05, 
+    theta.pr=0.5,       # Theta
+    b1.mu=0.30, 
+    b1.pr=0.5,              # Drift rate
+    bias.mu=0.75, 
+    bias.kappa=.25,        # Bias
     y_pred=y,                           # RT values
     .RNG.name="base::Super-Duper",      # Random Number Generator (Blair has had issues w this one (not so super))
-    .RNG.seed=67882))                   # Seed selected 
+    .RNG.seed=10002
+    ))                            # Seed selected 
   
-  # Initial parameters 2: 
   inits2 <- dump.format(list(
-    alpha.mu=1, alpha.pr=0.5,           # Alpha 
-    theta.mu=0.2,theta.pr=0.5,          # Theta 
-    b1.mu=0.05, b1.pr=0.1,              # Drift rate 
-    bias.mu=0.5,bias.kappa=.5, 
-    y_pred=y,  
+    alpha.mu=1.5, 
+    alpha.pr=.25,            # Alpha
+    theta.mu=0.150, 
+    theta.pr=0.3,       # Theta
+    b1.mu=0.01, 
+    b1.pr=0.20,              # Drift rate
+    bias.mu=0.5, 
+    bias.kappa=.5,        # Bias
+    y_pred=y,                           # RT values
     .RNG.name="base::Wichmann-Hill", 
-    .RNG.seed=1234))
+    .RNG.seed=67882
+  ))  
   
-  # Initial parameters 3: 
   inits3 <- dump.format(list(
-    alpha.mu=2.5, alpha.pr=0.5, 
-    theta.mu=0.3, theta.pr=0.05, 
-    b1.mu=0.1, b1.pr=0.01, 
-    bias.mu=0.6,bias.kappa=1, 
-    y_pred=y, 
+    alpha.mu=2.6, 
+    alpha.pr=.05,            # Alpha
+    theta.mu=0.14, 
+    theta.pr=0.7,       # Theta
+    b1.mu=0.7, 
+    b1.pr=0.3,              # Drift rate
+    bias.mu=0.25, 
+    bias.kappa=.75,        # Bias
+    y_pred=y,                           # RT values
     .RNG.name="base::Mersenne-Twister", 
     .RNG.seed=6666))
   
+  inits4 <- dump.format(list(
+    alpha.mu=2.00, 
+    alpha.pr=0.50,            # Alpha
+    theta.mu=0.130, 
+    theta.pr=0.5,       # Theta
+    b1.mu=0.16, 
+    b1.pr=0.10,              # Drift rate
+    bias.mu=0.52, 
+    bias.kappa=.3,        # Bias
+    y_pred=y,                           # RT values
+    .RNG.name="base::Super-Duper",      # Random Number Generator (Blair has had issues w this one (not so super))
+    .RNG.seed=999
+  ))                            # Seed selected 
+  
+  inits5 <- dump.format(list(
+    alpha.mu=1.0, 
+    alpha.pr=.25,            # Alpha
+    theta.mu=0.120, 
+    theta.pr=0.25,       # Theta
+    b1.mu=0.05, 
+    b1.pr=0.40,              # Drift rate
+    bias.mu=0.5, 
+    bias.kappa=.65,        # Bias
+    y_pred=y,                           # RT values
+    .RNG.name="base::Wichmann-Hill", 
+    .RNG.seed=333
+  ))  
+  
+  inits6 <- dump.format(list(
+    alpha.mu=2.5, 
+    alpha.pr=.75,            # Alpha
+    theta.mu=0.110, 
+    theta.pr=0.75,       # Theta
+    b1.mu=0.1, 
+    b1.pr=0.6,              # Drift rate
+    bias.mu=0.60, 
+    bias.kappa=.45,        # Bias
+    y_pred=y,                           # RT values
+    .RNG.name="base::Mersenne-Twister", 
+    .RNG.seed=222))
+  
+
+
   # Store results 
   Results <- run.jags(model = file.path(model_path,"M1_ug_drift.txt"), 
                       monitor=monitor, data=dat, n.chains=Chains,
-                      inits=c(inits1,inits2, inits3), 
-                      plots = TRUE, method="parallel", module="wiener",  ## Homework: Read about WIener process and 2d space
+                      inits=c(inits1,inits2, inits3, inits4,inits5,inits6), 
+                      plots = TRUE,
+                      method="parallel", 
+                      modules ="wiener",  
                       burnin=BurnIn,
                       sample=Sample,
                       thin=Thinning)
   
-  
-  Condition = genotype
-  
   # Save summary statistics
   Summary<-summary(Results)
   
-  # Goal for 
-  
   # Save model outputs and results for later analysis
-  # Generates file name based on group
-  # output_filename = paste(Device,Condition,Attempt_Date,Attempt_Num, sep="_")
-  output_filename = paste(paste(Condition,Attempt_Date,Attempt_Num, sep="_"), ".RData",sep="")
+  output_filename = paste(paste(genotype,Attempt_Date,Attempt_Num, sep="_"), ".RData",sep="")
   output_filepath = output_path / output_filename
   save(Results,Data,Summary, file=output_filepath) 
 }
 
-
-
-Data = Data
-  idxP = Data$idxP         # Sequentially numbered list of subject indices 
-  offer = Data$offer       # Pull offers 
-  rtpos = Data$rt          # Pull RTs (original, non-signed)
-  
-  # Modeling variables
-  y=Data$RT                # Signed RT values (to be predicted) 
-  N= length(y)                # Number of total trials
-  ns=length(unique(idxP))     # number of subjects (loops through for subject-level)
-  
-  # Prepare data in JAGS format 
-  dat <- dump.format(list(N=N, 
-                          y=y, 
-                          idxP=idxP, 
-                          offer=offer, 
-                          rt=rtpos, 
-                          ns=ns))
-  
-#   4b. Parameter initialization -----------------------------------------------
-
-  # Identify variables to track 
-  monitor = c("deviance",            # Come back to this 
-              # Group-level parameters (boundary, non-decision time, starting-point bias, effect of offer on drift rate)
-              "alpha.mu","theta.mu","bias.mu","b1.mu",
-              # Subject-level parameters
-              "alpha.p","theta.p","bias.p","b1.p")
-
-  # Run the JAGS model using M1_ug_drift.txt and parallel MCMC sampling 
-  # Tweak these last as needed, address model "settings" first 
-  # Change seed > values > RNG
-  # Keep the precisions > 0.01 (ideally ~ish 0.1~0.5)
-  
-  # Uniform priors and uninformed priors 
-  # Initial parameters 1:
-  
-  inits1 <- dump.format(list(
-    alpha.mu=2, alpha.pr=.5,            # Alpha
-    theta.mu=0.200, theta.pr=0.5,       # Theta
-    b1.mu=0.16, b1.pr=0.1,              # Drift rate
-    bias.mu=0.52, bias.kappa=.5,        # Bias
-    y_pred=y,                           # RT values
-    .RNG.name="base::Super-Duper",      # Random Number Generator (Blair has had issues w this one (not so super))
-    .RNG.seed=67882))                   # Seed selected 
-  
-  # Initial parameters 2: 
-  inits2 <- dump.format(list(
-    alpha.mu=1, alpha.pr=0.5,           # Alpha 
-    theta.mu=0.2,theta.pr=0.5,          # Theta 
-    b1.mu=0.05, b1.pr=0.1,              # Drift rate 
-    bias.mu=0.5,bias.kappa=.5, 
-    y_pred=y,  
-    .RNG.name="base::Wichmann-Hill", 
-    .RNG.seed=1234))
-  
-  # Initial parameters 3: 
-  inits3 <- dump.format(list(
-    alpha.mu=2.5, alpha.pr=0.5, 
-    theta.mu=0.3, theta.pr=0.05, 
-    b1.mu=0.1, b1.pr=0.01, 
-    bias.mu=0.6,bias.kappa=1, 
-    y_pred=y, 
-    .RNG.name="base::Mersenne-Twister", 
-    .RNG.seed=6666))
-  
-  # Store results 
-  Results <- run.jags(model = file.path(model_path,"M1_ug_drift.txt"), 
-                      monitor=monitor, data=dat, n.chains=Chains,
-                      inits=c(inits1,inits2, inits3), 
-                      plots = TRUE, method="parallel", module="wiener",  ## Homework: Read about WIener process and 2d space
-                      burnin=BurnIn,
-                      sample=Sample,
-                      thin=Thinning)
-
-  # Save summary statistics
-  Summary<-summary(Results)
-  
-  # Goal for 
-  
-  # Save model outputs and results for later analysis
-  # Generates file name based on group
-  output_filename = paste(Device,Condition,Attempt_Date,Attempt_Num, sep="_")
-  output_filename = "M1_params_HE.RData"
-  output_filepath = output_path / output_filename
-  save(results_WT2,Data_WT_attempt2,summary_stats_WT2, file=output_filepath) 
